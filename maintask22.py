74d0bdfe51d9f894fcb4bec36c299bb85dd8f583
corast <corastweb94@hotmail.com>
2018-04-05 23:54:47 +0200

    Adaboost almost complete

--- maintask22.py
+++ maintask22.py
@@ -2,6 +2,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_validate
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as pyplot
 
 #Setup the tree, so that we have the same type of tree for each itteration, which split on the largest entropy attribute.
 
@@ -18,55 +19,8 @@ adaboost_train = loadData("dataset/adaboost_train.csv")
 #Index 0 is the index, 1 is the y value, rest are attributes for classifying.
 
 
-
-def addaBoost(T, trainingData):
-    """ Perform boost with decision tree stumps
-    Return an matrix of weights assosiated with an classifier h, and the classifier(all trees in our case)"""
-    #store the weight assosiated with ech classifier
-    weights_classifier = []
-    #Seperate attributes from classification
-    Y = trainingData[:,0]
-    M = len(Y) #number of data entries.
-    X = trainingData[...,1:]
-
-    #We want to initialize the weights corresponding to m data.
-    sample_w = np.array([1/M]*M) #first element correspond to first data row etc.
-
-    for t in range(T):
-        
-        pass
-        
-    tree = dtree.fit(X,Y,sample_weight=sample_w)
-    
-    prediction = tree.predict(X)
-    #We need to take the prediction and figure out how many are wrong, and adjust those
-    E_wrong = np.array([int(i) for i in (prediction != Y)]) #create list of which index is wrong
-    E_correct = np.array([int(i) for i in (prediction == Y)]) #create list of which index is wrong
-
-    #We know that the sum of this one represent the number e
-    e_t = sum(E_wrong)/M #number of mistakes/ number of data entries.
-
-    alpha_t = 1/2 * np.log((1-e_t)/e_t) 
-    weights_classifier.append(alpha_t) #store the weight assosiated with this classifier.
-
-    #Need to update the sample_weights for next itteration.
-
-    Z_2 = 0
-    for m, weight in enumerate(sample_w):
-        #Need to calculate
-        Z_2 += weight*np.exp(-alpha_t*prediction[m]*Y[m])
-        pass
-    print("Z_2 {}".format(Z_2))
-    sample_test = np.array(list(sample_w)) #Create copy
-    print(sample_test)
-    for m in range(M):
-        sample_test[m] = sample_test[m]*np.exp(-alpha_t*prediction[m]*Y[m])/Z_2
-    print(sum(sample_test))
-
-    #We need to know how much to change the weights.
-    Z = sum([w*np.exp(alpha_t*Y[i]*prediction[i]) for i,w in enumerate(sample_w)])
-    #print(Z)
-
+def calculateWeights(M,sample_w, prediction,E_wrong, E_correct):
+    """ Update weights """
     #From wikipedia: Sum_weigths_correct_t+1/Sum_weigths_wrong_t+1 = Sum_weigths_correct_t/Sum_weigths_wrong_t
     #Which simplifies calculating the new weights.
     #Weights of all correct is 1/2, weight of all wrong is 1/2 as well.
@@ -78,42 +32,104 @@ def addaBoost(T, trainingData):
     W_correct = Z_c/sum(E_correct)
     W_wrong = Z_w/sum(E_wrong)
 
-    Z_c = sum(np.multiply(sample_w,E_correct))
-    Z_w = sum(np.multiply(sample_w,E_wrong))
-    
-    #print(Y)
-    #print(tree.tree_.__getstate__()['nodes'])
     for i,wrong in enumerate(E_wrong):
         if(wrong == 1):
-            sample_w[i] == sample_w[i]+W_wrong
+            sample_w[i] = sample_w[i]+W_wrong
         else:
-            sample_w[i] == sample_w[i]-W_correct
-        
-    print(sum(sample_w))
-    print(sample_w)
-
-
-addaBoost(100, adaboost_train)
-
-def calculateWeights(M,sample_w, prediction,E_wrong, E_correct):
-    """ Update weights """
-    Z_c = sum(np.multiply(sample_w,E_correct))
-    Z_w = sum(np.multiply(sample_w,E_wrong))
-
-    #difference in the new weights
-    W_correct = Z_c/sum(E_correct)
-    W_wrong = Z_w/sum(E_wrong)
-
-    Z_c = sum(np.multiply(sample_w,E_correct))
-    Z_w = sum(np.multiply(sample_w,E_wrong))
-
-    for i,wrong in enumerate(E_wrong):
-        if(wrong == 1):
-            sample_w[i] == sample_w[i]+W_wrong
-        else:
-            sample_w[i] == sample_w[i]-W_correct
-
+            sample_w[i] = sample_w[i]-W_correct
+    #print(sum(sample_w))
     return sample_w
 
-def calculateWeightsTwo(sample_w, Z):
-    pass
+def calculateWeightsTwo(M,sample_w, alpha_t,predictions,Y):
+    """ Old method  """
+    Z = 0
+    for m, weight in enumerate(sample_w):
+        #Need to calculate
+        Z += weight*np.exp(-alpha_t*predictions[m]*Y[m])
+    
+    #for m in range(M):
+    #    sample_w[m] = (sample_w[m]*np.exp(-alpha_t*predictions[m]*Y[m]))/Z
+    #print(sum(sample_w))
+    A = np.multiply(Y,-alpha_t)
+    B = np.multiply(predictions,A)
+    C = np.multiply(B,Z)
+    D = np.exp(C)
+    sample_w = np.multiply(sample_w, D)
+    #sample_w = np.multiply(sample_w,np.exp(-alpha_t*predictions*Y),Z)
+    return sample_w
+
+
+def addaBoost(T, trainingData, testData):
+    """ Perform boost with decision tree stumps
+    Return an matrix of weights assosiated with an classifier h, and the classifier(all trees in our case)"""
+
+    #To test tree later on.
+    Y_test = testData[:,0]
+    X_test = testData[...,1:]
+    #store the weight assosiated with ech classifier
+    weights_classifier = []
+    classification_predictions = [] #Store the prediction of every tree.
+    #Seperate attributes from classification
+    Y = trainingData[:,0]
+    M = len(Y) #number of data entries.
+    X = trainingData[...,1:]
+
+    #We want to initialize the weights corresponding to m data.
+    sample_w = np.array([1/M]*M) #first element correspond to first data row etc.
+
+    for t in range(T):
+        tree = dtree.fit(X,Y,sample_weight=sample_w)
+    
+        predictions = tree.predict(X)
+        #predictions2 = tree.predict(X_test)
+        #We need to take the prediction and figure out how many are wrong, and adjust those
+        E_wrong = np.array([int(i) for i in (predictions != Y)]) #create list of which index is wrong
+        E_correct = np.array([int(i) for i in (predictions == Y)]) #create list of which index is wrong
+
+        #We know that the sum of this one represent the number e
+        e_t = sum(E_wrong)/M #number of mistakes/ number of data entries.
+
+        alpha_t = 1/2 * np.log((1-e_t)/e_t) 
+        weights_classifier.append([alpha_t]) #store the weight assosiated with this classifier.
+
+        #sample_w = calculateWeights(M,sample_w,predictions,E_wrong,E_correct)
+        sample_w = calculateWeightsTwo(M,sample_w,alpha_t,predictions,Y)
+        classification_predictions.append(tree.predict(X_test))
+        #We test the tree in the current data.
+        #store ever prediction of every classifier.
+
+        predictionMatrix = []
+
+        for prediction in classification_predictions:
+            #Check the result thus far for every iteration.
+            predictionMatrix.append(list(prediction))
+        
+        predictionMatrix = np.asarray(predictionMatrix)
+        weightMatrix = np.asarray(weights_classifier)
+        #print("new line")
+        #calculate the sum of a_t*f_t(x)
+        guess = np.multiply(weightMatrix, predictionMatrix)
+        guess_T = np.transpose(guess)
+
+        #figure out the final verdict from the trees.
+        sign_predictions = np.sign(guess_T.sum(axis=1))
+        #print(sign_predictions)
+        #print(Y_test)
+        Error = np.sum(sign_predictions != Y_test)
+        Correct = np.sum(sign_predictions == Y_test)
+        #print(Error+Correct)
+        #print(correct)
+        Error_rate = Error/(Error + Correct) * 100
+        pyplot.plot(t,Error_rate,'-o')
+        print("Error rate itteration {} is {}%".format(t, Error_rate))
+        #Calculate the error from this.
+    pyplot.show()
+    return weights_classifier
+
+adaboost_test = loadData("dataset/adaboost_test.csv")
+weights = addaBoost(400, adaboost_train, adaboost_test)
+#Run som tests
+
+
+
+#print(classifiers)
